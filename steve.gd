4b330a71c3a8032afef9be031534ae42c8e71cc1
Aleksey Smirnov <debugger94@gmail.com>
2022-05-28 20:54:04 +0300

    Part 4

--- steve.gd
+++ steve.gd
@@ -7,21 +7,29 @@ const JUMP_VELOCITY = -600.0
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var animated_sprite := $AnimatedSprite as AnimatedSprite2D
+
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
+	
 	if direction:
+		animated_sprite.flip_h = direction < 0
+		animated_sprite.play("walk")
 		velocity.x = direction * SPEED
 	else:
+		animated_sprite.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	if not is_on_floor():
+		# Add the gravity.
+		animated_sprite.play("jump")
+		velocity.y += gravity * delta
+	else:
+		# Handle Jump.
+		if Input.is_action_just_pressed("jump"):
+			animated_sprite.flip_h = direction < 0
+			velocity.y = JUMP_VELOCITY
+		
 	move_and_slide()
